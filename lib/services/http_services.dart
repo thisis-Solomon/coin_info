009e193ce16f4150f8ebfee8e13909d5138a69a8
Solomon Njobvu <solomonnjobvu1@gmail.com>
2025-08-25 19:45:12 +0200

    feat: Implement GET request method in HTTPServices
    This commit adds a `get` method to the `HTTPServices` class.
    
    The `get` method:
    - Takes a `_path` string as an argument.
    - Constructs the full URL by appending the `_path` to the `_baseUrl` (obtained from `AppConfig`).
    - Uses the `dio` instance to make an HTTP GET request to the constructed URL.
    - Returns a `Future<Response?>`.
    - Includes basic error handling that prints a message and the error to the console if the request fails.
    
    The constructor no longer prints the `_baseUrl`.

--- lib/services/http_services.dart
+++ lib/services/http_services.dart
@@ -10,6 +10,17 @@ class HTTPServices{
   HTTPServices(){
     _appConfig = GetIt.instance.get<AppConfig>();
     _baseUrl = _appConfig!.COIN_API_BASE_URL;
-    print(_baseUrl);
+  }
+
+  Future<Response?> get(String _path) async{
+   try{
+     String _url = "$_baseUrl$_path";
+     Response _response =   await dio.get(_url);
+
+     return _response;
+   }catch(e){
+     print("HTTPService: Failed to get the response");
+     print(e);
+   };
   }
 }
